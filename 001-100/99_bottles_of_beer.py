ef425fc8cac72d451a093e8d606d16cd9f3874d3
Madhvi Ń <madhvinandanwar@gmail.com>
2021-09-13 15:18:38 +0530

    99 bottles of beer

---
+++ 001-100/99_bottles_of_beer.py
@@ -0,0 +1,25 @@
+"""
+Display the complete lyrics for the song: 99 Bottles of Beer on the Wall.
+
+The lyrics follow this form:
+
+99 bottles of beer on the wall
+99 bottles of beer
+Take one down, pass it around
+98 bottles of beer on the wall
+
+98 bottles of beer on the wall
+98 bottles of beer
+Take one down, pass it around
+
+"""
+
+if __name__ == '__main__':
+    max = 99
+
+    while max > 1:
+        print(f"{max} bottles of beer on the wall")
+        print(f"{max} bottles of beer")
+        print(f"Take one down, pass it around")
+        max -= 1
+        print(f"{max} bottles of beer \n")
