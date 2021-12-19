fca76f7f65b814d614b21a8cf66a1fac99ea51ff
Madhvi Ń <madhvinandanwar@gmail.com>
2021-12-19 12:15:49 +0530

    100 doors

---
+++ 01 - 09/21_game.py
@@ -0,0 +1,11 @@
+"""
+21 is a two player game, the game is played by choosing a number (1, 2, or 3) to be added to the running total.
+
+The game is won by the player whose chosen number causes the running total to reach exactly 21.
+
+The running total starts at zero. One player will be the computer.
+
+Players alternate supplying a number to be added to the running total.
+"""
+
+if __name__ == '__main__':
